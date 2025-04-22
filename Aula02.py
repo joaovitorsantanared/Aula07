64b78aee0826e10badcdbe9bfd8b6c6686456054
joaosantanared <jvtreloso@gmail.com>
2025-04-22 14:53:28 -0300

    Numero par, impar, positivo e negativo

--- Aula02.py
+++ Aula02.py
@@ -1,16 +1,23 @@
+
 N= int(input("Digite o número"))
 
-if N >0 and N %2 ==0:
-    print("Numero Positivo e Par")
+repetir = "sim"
+while repetir =="sim":
 
-elif N <0 and N %2 ==1:
-    print("Numero Negativo e Impar")
 
-elif N >0 and N %2 ==1:
-    print("Numero Positivo e Impar")
+    if N >0 and N %2 ==0:
+        print("Numero Positivo e Par")
 
-elif N <0 and N %2 ==0:
-    print("Numero Negativo e Par")
+    elif N <0 and N %2 ==1:
+        print("Numero Negativo e Impar")
 
-else:
-    print("Numero inexistente") 
+    elif N >0 and N %2 ==1:
+        print("Numero Positivo e Impar")
+
+    elif N <0 and N %2 ==0:
+        print("Numero Negativo e Par")
+
+    else:
+        print("Numero inexistente")
+
+    repetir = input("Deseja repetir novamente? Digite sim para continuar")
