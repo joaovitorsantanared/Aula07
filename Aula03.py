49018ad700ac0454352d834fa9a8256f7c78b7bf
joaosantanared <jvtreloso@gmail.com>
2025-04-22 15:49:29 -0300

    salariominimo

---
+++ Aula03.py
@@ -0,0 +1,8 @@
+
+SalarioM= (float(input("Digite o valor do salario minimo atual:")))
+
+Salario= float(input("Digite o seu salário"))
+
+ESalarioM = Salario/SalarioM
+
+print(f"o Seu salário é equivalente a:{ESalarioM} salarios minimos")
